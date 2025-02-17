5719e8bc3116e9e9b600fc28fc99e3f7f5ab4865
Olga <domagile@gmail.com>
2025-02-17 20:51:44 +0100

    Some Todo fixed

--- tests/test_contact.py
+++ tests/test_contact.py
@@ -8,45 +8,23 @@ from conftest import create_headers, contact_data_2
 def test_add_contact(base_url, contact_data_1, register_user):
     user_data, token = register_user
 
-    response = requests.post(f"{base_url}/contacts", json=contact_data_1, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
-
+    response = create_contact(base_url, contact_data_1, token)
     actual_user_data = response.json()
-    check_user_data(actual_user_data, contact_data_1, user_data.get("_id"), response.text)
+    check_contact_data(actual_user_data, contact_data_1, user_data.get("_id"), response.text)
 
-
-def check_user_data(actual_user_data, expected_user_data, user_id, response_text):
-    assert "_id" in actual_user_data, f"Missing id: {response_text}"
-    assert actual_user_data["firstName"] == expected_user_data["firstName"], "Error: firstName does not match"
-    assert actual_user_data["lastName"] == expected_user_data["lastName"], "Error: lastName does not match"
-    assert actual_user_data["birthdate"] == expected_user_data["birthdate"], "Error: birthdate does not match"
-    assert actual_user_data["email"] == expected_user_data["email"], "Error: email does not match"
-    assert actual_user_data["phone"] == expected_user_data["phone"], "Error: phone does not match"
-    assert actual_user_data["street1"] == expected_user_data["street1"], "Error: street1 does not match"
-    assert actual_user_data["street2"] == expected_user_data["street2"], "Error: street2 does not match"
-    assert actual_user_data["city"] == expected_user_data["city"], "Error: city does not match"
-    assert actual_user_data["stateProvince"] == expected_user_data["stateProvince"], "Error: stateProvince does not match"
-    assert actual_user_data["postalCode"] == expected_user_data["postalCode"], "Error: postalCode does not match"
-    assert actual_user_data["country"] == expected_user_data["country"], "Error: country does not match"
-    assert actual_user_data["owner"] == user_id, "Error: owner does not match"
-    assert "__v" in actual_user_data, f"Missing version: {response_text}"
-
-
-#todo remove duplication of contact creation and check
 def test_get_contact_list(base_url, contact_data_1, contact_data_2, contact_data_3, register_user):
     user_data, token = register_user
-    response = requests.post(f"{base_url}/contacts", json=contact_data_1, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
+    user_id = user_data.get("_id")
+
+    response = create_contact(base_url, contact_data_1, token)
     expected_contact_1_id = response.json().get("_id")
     print(f"Expected contact 1 id: {expected_contact_1_id}")
 
-    response = requests.post(f"{base_url}/contacts", json=contact_data_2, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
+    response = create_contact(base_url, contact_data_2, token)
     expected_contact_2_id = response.json().get("_id")
     print(f"Expected contact 2 id: {expected_contact_2_id}")
 
-    response = requests.post(f"{base_url}/contacts", json=contact_data_3, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
+    response = create_contact(base_url, contact_data_3, token)
     expected_contact_3_id = response.json().get("_id")
     print(f"Expected contact 3 id: {expected_contact_3_id}")
 
@@ -56,63 +34,50 @@ def test_get_contact_list(base_url, contact_data_1, contact_data_2, contact_data
 
     actual_contact_1 = next((contact for contact in contact_list_response.json() if contact.get("_id") == expected_contact_1_id), None)
     assert actual_contact_1 is not None, "Contact is not found"
-
-    owner = actual_contact_1.get("owner")
-    user_id = user_data.get("_id")
-    assert owner == user_id, "Incorrect owner"
-    compare_contacts(actual_contact_1, contact_data_1)
+    check_contact_data(actual_contact_1, contact_data_1, user_id, contact_list_response.text)
 
     actual_contact_2 = next((contact for contact in contact_list_response.json() if contact.get("_id") == expected_contact_2_id), None)
     assert actual_contact_2 is not None, "Contact is not found"
-
-    owner = actual_contact_2.get("owner")
-    user_id = user_data.get("_id")
-    assert owner == user_id
-    compare_contacts(actual_contact_2, contact_data_2)
+    check_contact_data(actual_contact_2, contact_data_2, user_id, contact_list_response.text)
 
     actual_contact_3 = next((contact for contact in contact_list_response.json() if contact.get("_id") == expected_contact_3_id), None)
     assert actual_contact_3 is not None, "Contact is not found"
-
-    owner = actual_contact_3.get("owner")
-    user_id = user_data.get("_id")
-    assert owner == user_id
-    compare_contacts(actual_contact_3, contact_data_3)
+    check_contact_data(actual_contact_3, contact_data_3, user_id, contact_list_response.text)
 
 #todo move duplicate code to method
 def test_get_contact(base_url, contact_data_1, contact_data_2, contact_data_3, register_user):
-    _, token = register_user
-    response = requests.post(f"{base_url}/contacts", json=contact_data_1, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
-    response = requests.post(f"{base_url}/contacts", json=contact_data_2, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
+    user_data, token = register_user
+
+    response = create_contact(base_url, contact_data_1, token)
+
+    response = create_contact(base_url, contact_data_2, token)
     contact_2_id = response.json().get("_id")
-    response = requests.post(f"{base_url}/contacts", json=contact_data_3, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
+
+    response = create_contact(base_url, contact_data_3, token)
 
     response = requests.get(f"{base_url}/contacts/{contact_2_id}", headers=create_headers(token))
     assert response.status_code == HTTPStatus.OK, f"Error [get contact]: {response.text}"
     actual_contact_2 = response.json()
-    compare_contacts(actual_contact_2, contact_data_2)
+    check_contact_data(actual_contact_2, contact_data_2, user_data.get("_id"), response.text)
 
 def test_update_contact_put(base_url, contact_data_1, contact_data_2, register_user):
-    _, token = register_user
-    response = requests.post(f"{base_url}/contacts", json=contact_data_1, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
-    response = requests.post(f"{base_url}/contacts", json=contact_data_1, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
+    user_data, token = register_user
+
+    response = create_contact(base_url, contact_data_1, token)
+
+    response = create_contact(base_url, contact_data_1, token)
     contact_id = response.json().get("_id")
 
     response = requests.put(f"{base_url}/contacts/{contact_id}", json=contact_data_2, headers=create_headers(token))
     assert response.status_code == HTTPStatus.OK, f"Error [update contact put]: {response.text}"
     actual_contact_data = response.json()
-    compare_contacts(actual_contact_data, contact_data_2)
+    check_contact_data(actual_contact_data, contact_data_2, user_data.get("_id"), response.text)
 
 def test_update_contact_patch(base_url, contact_data_1, register_user):
     _, token = register_user
-    response = requests.post(f"{base_url}/contacts", json=contact_data_1, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
-    response = requests.post(f"{base_url}/contacts", json=contact_data_1, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
+
+    response = create_contact(base_url, contact_data_1, token)
+    response = create_contact(base_url, contact_data_1, token)
     contact_id = response.json().get("_id")
 
     updated_field_json = {"firstName": "Anna"}
@@ -121,10 +86,12 @@ def test_update_contact_patch(base_url, contact_data_1, register_user):
     actual_contact_data = response.json()
     assert actual_contact_data["firstName"] == updated_field_json["firstName"], "Error: firstName was not updated"
 
+
+
 def test_delete_contact_(base_url, contact_data_1, register_user):
     _, token = register_user
-    response = requests.post(f"{base_url}/contacts", json=contact_data_1, headers=create_headers(token))
-    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
+
+    response = create_contact(base_url, contact_data_1, token)
     contact_id = response.json().get("_id")
 
     response = requests.delete(f"{base_url}/contacts/{contact_id}", headers=create_headers(token))
@@ -176,6 +143,7 @@ def test_delete_contact_(base_url, contact_data_1, register_user):
     ])
 def test_add_contact_negative(base_url, register_user, contact_data, expected_status, error_message_part):
     _, token = register_user
+
     response = requests.post(f"{base_url}/contacts", json=contact_data, headers=create_headers(token))
     assert response.status_code == HTTPStatus.BAD_REQUEST, f"Error [add contact]: {response.status_code}"
     actual_error_message = response.json().get("message")
@@ -198,7 +166,13 @@ def test_update_contact_with_invalid_id_patch(base_url, contact_data_1, register
     response = requests.patch(f"{base_url}/contacts/{contact_id}", json=updated_field_json, headers=create_headers(token))
     assert response.status_code == HTTPStatus.BAD_REQUEST, f"Error [update contact patch]: {response.text}"
 
-def compare_contacts(actual_data, expected_data):
+def create_contact(base_url, contact_data, token):
+    response = requests.post(f"{base_url}/contacts", json=contact_data, headers=create_headers(token))
+    assert response.status_code == HTTPStatus.CREATED, f"Error [add contact]: {response.status_code}"
+    return response
+
+def check_contact_data(actual_data, expected_data, user_id, response_text):
+    assert "_id" in actual_data, f"Missing id: {response_text}"
     assert actual_data["firstName"] == expected_data["firstName"], "Error: firstName does not match"
     assert actual_data["lastName"] == expected_data["lastName"], "Error: lastName does not match"
     assert actual_data["birthdate"] == expected_data["birthdate"], "Error: birthdate does not match"
@@ -210,3 +184,5 @@ def compare_contacts(actual_data, expected_data):
     assert actual_data["stateProvince"] == expected_data["stateProvince"], "Error: stateProvince does not match"
     assert actual_data["postalCode"] == expected_data["postalCode"], "Error: postalCode does not match"
     assert actual_data["country"] == expected_data["country"], "Error: country does not match"
+    assert actual_data["owner"] == user_id, "Error: owner does not match"
+    assert "__v" in actual_data, f"Missing version: {response_text}"
